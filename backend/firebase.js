b00c8c70734da2b4e24d66f63c1e5fdba0e94e43
Ankit Mondal <183182926+itz-ankit-425@users.noreply.github.com>
2026-06-30 12:09:16 +0530

    Use env var for Firebase credentials

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,8 +1,16 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { readFileSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
 
-const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json", "utf8"));
+}
 
 initializeApp({
   credential: cert(serviceAccount),
